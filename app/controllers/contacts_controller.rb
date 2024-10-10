0a088045847ac8b856bc815dda6d705ae5575d6f
mark <mmarkmarkm0@gmail.com>
2024-10-11 01:01:59 +0300

    updating a contact:

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,5 @@
 class ContactsController < ApplicationController
-  before_action :set_category, only: [:new, :create, :show]
+  before_action :set_category, only: [:new, :create, :show, :edit, :update]
 
   def show
     @contact = Contact.find(params[:id])
@@ -19,14 +19,27 @@ class ContactsController < ApplicationController
     end
   end
 
+  def edit
+    @contact = @category.contacts.find(params[:id])  
+  end
+
+  def update
+    @contact = @category.contacts.find(params[:id])  
+    if @contact.update(contact_params)
+      redirect_to category_contacts_path(@category), notice: 'Contact was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
 
   private
 
   def set_category
-    @category = Category.find(params[:category_id])  # Set the category based on the params
+    @category = Category.find(params[:category_id])  
   end
 
   def contact_params
-    params.require(:contact).permit(:firstname, :email, :phonenumber)  # Adjust according to your contact attributes
+    params.require(:contact).permit(:firstname, :email, :phonenumber)  
   end
 end
